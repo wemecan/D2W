0400cc0c6db0306cb086a8647a048e3c176f1b3d
Administrator <killnetsec@gmail.com>
2019-04-12 15:31:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## D2W - Discuz 和 WordPress 实现数据互相迁移
+
+主要功能如下
+
+- 用户可以通过Discuz数据将自身数据导入到WordPress，平滑迁移
+- 用户可以通过WordPress数据将自身用户数据平滑迁移到Discuz中
+
+目前仍在开发中，上传至Github仅做代码仓库使用
---
+++ app/Http/Controllers/d2w/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ai0by
+ * Date: 2019/4/12
+ * Time: 14:59
+ */
+
+namespace App\Http\Controllers\d2w;
+
+use App\Http\Controllers\Controller;
+use Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Input;
+use DB;
+
+class IndexController extends Controller {
+    public function __construct(){
+        parent::__construct();
+    }
+
+}
